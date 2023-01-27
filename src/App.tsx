ceb154916ee8f76b82c09858d34cd7dcc5b227f2
Bryan <bwmaravilla_14@hotmail.com>
2023-01-27 17:40:02 -0500

    updated images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import { Card } from 'types/card.model';
+import { Image } from 'types/image.model';
 import './App.css';
 
+const cardImages: Image[] = [
+  { "src": "/img/kaido.jpg" },
+  { "src": "/img/luffy.jpg" },
+  { "src": "/img/shanks.jpg" },
+  { "src": "/img/teach.jpeg" },
+  { "src": "/img/white-beard.jpg" },
+  { "src": "/img/zoro.jpg" },
+]
+
 function App() {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [turns, setTurns] = useState<Number>(0);
+
+  const shuffleCards = (): void => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() })) as Card[];
+
+    setCards(shuffledCards);
+    setTurns(0);
+  }
+
+  console.log(cards, turns)
   return (
     <div className="App">
       <h1>Memory Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
